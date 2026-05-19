86b2724437a30d9331f3d5733acd8596052d01cb
jaymtwoo-ai <jaymtwoo@gmail.com>
2026-05-19 16:21:19 +0900

    fix: guide repository UUID 처리 및 security Bearer 토큰 방식 추가

--- app/dependencies/security.py
+++ app/dependencies/security.py
@@ -1,17 +1,21 @@
-from typing import Annotated
-
-from fastapi import Depends, HTTPException, status
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-
-from app.models.users import User
-from app.repositories.user_repository import UserRepository
+﻿from fastapi import Depends, HTTPException, status, Request
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from app.services.jwt import JwtService
+from app.repositories.user_repository import UserRepository
 
-security = HTTPBearer()
+security = HTTPBearer(auto_error=False)
 
-
-async def get_request_user(credential: Annotated[HTTPAuthorizationCredentials, Depends(security)]) -> User:
-    token = credential.credentials
+async def get_request_user(
+    request: Request,
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+):
+    token = None
+    if credentials:
+        token = credentials.credentials
+    else:
+        token = request.cookies.get("access_token")
+    if not token:
+        raise HTTPException(detail="Token is missing.", status_code=status.HTTP_401_UNAUTHORIZED)
     verified = JwtService().verify_jwt(token=token, token_type="access")
     user_id = verified.payload["user_id"]
     user = await UserRepository().get_user(user_id)
