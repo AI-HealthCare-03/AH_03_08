c30284d1a994b2bc176e8ac1dca7644deccb3ef9
Jun-0001 <jtj73872711@gmail.com>
2026-05-22 16:38:37 +0900

    style: ruff lint 오류 수정

--- ai_worker/tasks/__init__.py
+++ ai_worker/tasks/__init__.py
@@ -1,4 +1,4 @@
-from ai_worker.tasks.llm_task import generate_guide
 from ai_worker.tasks.image_task import classify_pill
+from ai_worker.tasks.llm_task import generate_guide
 
 __all__ = ["generate_guide", "classify_pill"]
