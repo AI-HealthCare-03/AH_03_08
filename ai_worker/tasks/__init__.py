710e09f5c30c81c683545f7a719d5b4124aa6c2e
Jun-0001 <jtj73872711@gmail.com>
2026-05-22 13:41:22 +0900

    fix: image classify_pill Celery 태스크 등록 추가

--- ai_worker/tasks/__init__.py
+++ ai_worker/tasks/__init__.py
@@ -1,3 +1,4 @@
 from ai_worker.tasks.llm_task import generate_guide
+from ai_worker.tasks.image_task import classify_pill
 
-__all__ = ["generate_guide"]
+__all__ = ["generate_guide", "classify_pill"]
