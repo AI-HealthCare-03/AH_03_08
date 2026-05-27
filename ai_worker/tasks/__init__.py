25f183b7284fc289e9d3b5ed5269846107f5ae78
Jun-0001 <jtj73872711@gmail.com>
2026-05-27 13:15:05 +0900

    fix: tasks/__init__.py generate_guide → generate_guide_task 수정

--- ai_worker/tasks/__init__.py
+++ ai_worker/tasks/__init__.py
@@ -1,5 +1,5 @@
 from ai_worker.tasks.image_task import classify_pill
-from ai_worker.tasks.llm_task import generate_guide
+from ai_worker.tasks.llm_task import generate_guide_task
 from ai_worker.tasks.tts_task import generate_tts_task
 
-__all__ = ["generate_guide", "classify_pill", "generate_tts_task"]
+__all__ = ["generate_guide_task", "classify_pill", "generate_tts_task"]
