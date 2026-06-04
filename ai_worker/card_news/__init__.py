aee6a0be4bd086e03bcb952357e267ff1f144cd9
Jun-0001 <jtj73872711@gmail.com>
2026-06-04 15:26:02 +0900

    refactor: 카드뉴스 generator에서 S3 파라미터 제거

--- ai_worker/card_news/__init__.py
+++ ai_worker/card_news/__init__.py
@@ -3,9 +3,4 @@ from ai_worker.core.config import Config
 
 
 def get_card_news_generator(config: Config) -> CardNewsGenerator:
-    return CardNewsGenerator(
-        bucket_name=config.S3_BUCKET_NAME,
-        aws_access_key=config.AWS_ACCESS_KEY,
-        aws_secret_key=config.AWS_SECRET_KEY,
-        aws_region=config.AWS_REGION,
-    )
+    return CardNewsGenerator()
