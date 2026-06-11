2a9bc456fa0773400f1327cb558cd1d350b2115b
Jun-0001 <jtj73872711@gmail.com>
2026-06-11 14:12:49 +0900

    feat: get_analysis_result 반환값에 color_class2 필드 추가

--- app/apis/v1/image_routers.py
+++ app/apis/v1/image_routers.py
@@ -52,6 +52,7 @@ async def get_analysis_result(
             dl_material=drug_info.get("dl_material"),
             drug_shape=drug_info.get("drug_shape"),
             color_class1=drug_info.get("color_class1"),
+            color_class2=drug_info.get("color_class2"),
             di_class_no=drug_info.get("di_class_no"),
             di_etc_otc_code=drug_info.get("di_etc_otc_code"),
             chart=drug_info.get("chart"),
