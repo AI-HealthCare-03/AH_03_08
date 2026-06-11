3bd943aaf720f46b98c7fbdd563a9af1ea0229d0
Jun-0001 <jtj73872711@gmail.com>
2026-06-11 13:19:45 +0900

    feat: pill-match 응답에 color_class1, drug_shape, chart 필드 추가 및 인덱스 재생성

--- app/apis/v1/image_routers.py
+++ app/apis/v1/image_routers.py
@@ -111,4 +111,7 @@ async def match_pill_by_ocr(
         di_etc_otc_code=info.get("di_etc_otc_code"),
         print_front=info.get("print_front"),
         print_back=info.get("print_back"),
+        color_class1=info.get("color_class1"),
+        drug_shape=info.get("drug_shape"),
+        chart=info.get("chart"),
     )
